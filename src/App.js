942b8e720bb80f9069d552e36ac3746f5e540c9e
viraoka <verakazyun@gmail.com>
2023-10-24 13:32:20 +0300

    modified app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from './redux/counter';
-
+import { decrement, increment, incrementByAmount, decrementByAmount, multiplyByAmount } from './redux/counter';
 
 
 function App() {
 
-  const count = useSelector((state) => state.counter);
+  const { count } = useSelector((state) => state.apple);
+  const dispatch = useDispatch()
+
 
   return (
     <div>
       <p>Score: {count}</p>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+      <button onClick={() => dispatch(decrementByAmount(5))}>-5</button>
+      <button onClick={() => dispatch(multiplyByAmount(5))}>*5</button>
     </div>
   );
 }
